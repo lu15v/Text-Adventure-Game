b22a23ae2cd3c030434d6a202a7a4e9328d5d84f
Fernando Gómez <gomezhyuuga@gmail.com>
2016-05-04 00:20:29 -0500

    to_s in Game and Player

--- src/models/game.rb
+++ src/models/game.rb
@@ -1,4 +1,13 @@
 class Game
-  def initialize(state, player, current_room)
+  attr_accessor :player, :current_room, :state
+
+  def initialize(player)
+    @player = player
+    @current_room = InitialRoom
+    @state = ExploringState
+  end
+
+  def to_s
+    "Player: #{player.name} \nCurrent Room: #{@current_room} \nState: #{@state}"
   end
 end
--- src/models/player.rb
+++ src/models/player.rb
@@ -1,12 +1,21 @@
 class Player
-  attr_accessible :strength, :wealth, :food, :tally, :monsters_killed, :items
+  attr_accessor :name, :strength, :wealth, :food, :tally, :monsters_killed, :items
 
-  def initialize
+  def initialize(name)
+    @name            = name
     @strength        = 60 + rand(1..100) # LINE 2620
     @wealth          = 30 + rand(1..100)
     @food            = 0
     @tally           = 0
     @monsters_killed = 0
     @items           = []
+  end
+
+  def to_s
+    %Q{
+      Your strength is #{@strength}.
+      You have $#{@wealth}.
+      Your provision sack holds #{@food} units of food.
+    }
   end
 end
