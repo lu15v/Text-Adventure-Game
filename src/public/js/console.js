ee431b09677c62d0600ecc92b8c26b282c6c8244
Fernando Gómez <gomezhyuuga@gmail.com>
2016-05-05 08:26:14 -0500

    Front-end base

---
+++ src/public/js/console.js
@@ -0,0 +1,36 @@
+$(function() {
+  $('.type-text' ).each( function() {
+    var items = $( this ).text();
+    items = items.split('.');
+    var data = items.map(function(el) {
+      return el + ".\\n";
+    });
+    var thing = $( this ).empty().attr( 'title', '' ).teletype( {
+      text: data,
+      typeDelay: 10,
+      backDelay: 10,
+      delay: 10,
+      preserve: true,
+      cursor: '▋',
+      loop: 1,
+      humanise: false,
+    });
+  });
+
+  $('body').click(function() {
+    $('#user_input').focus();
+  });
+
+  $('#command-form').submit(sendCommand);
+});
+
+
+function sendCommand(event) {
+  event.preventDefault();
+  var input = $('#user_input');
+  var command = input.val();
+  console.log("Sending command " + command);
+
+  // TODO: send command to the server
+  input.val('');
+}
