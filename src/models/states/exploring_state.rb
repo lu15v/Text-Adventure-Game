b00646653647081c509465635efee35cecd80dba
Fernando Gómez <gomezhyuuga@gmail.com>
2016-05-05 02:33:53 -0500

    ExploringState#pick_up
    TODO: update game rooms status to set that the treasure has already
    picked up from the room

--- src/models/states/exploring_state.rb
+++ src/models/states/exploring_state.rb
@@ -28,4 +28,18 @@ class ExploringState
     return self.magic if room.name == "Entrance" || room.name == "Exit"
     @game.current_room = room.name
   end
+
+  # Pick-up the treasure in the room if there is any
+  def pick_up
+    treasure = @game.current_room_model.treasure
+    has_torch = @game.player.has_torch?
+    return puts "There is no treasure to pick up" unless treasure && treasure > 0
+    return puts "You cannot see where it is" unless has_torch
+
+    @game.player.wealth += treasure
+
+    return puts "You picked-up gems worth $#{treasure}"
+
+    # TODO: update room status
+  end
 end
