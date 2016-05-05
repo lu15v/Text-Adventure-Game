9289943a17f640d5ea5cc75c5f1435d47a6c315a
Fernando Gómez <gomezhyuuga@gmail.com>
2016-05-05 01:03:59 -0500

    Sandbox: require game and player

--- src/models/sandbox.rb
+++ src/models/sandbox.rb
@@ -6,6 +6,8 @@ require 'sequel'
 require 'byebug'
 DB = Sequel.connect("sqlite://game.db") # in memory
 
+require_relative 'game'
+require_relative 'player'
 require_relative 'monster'
 require_relative 'room'
 require_relative 'movement'
