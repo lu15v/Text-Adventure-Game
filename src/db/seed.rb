c78b1ce4fbe12932f0fc79d5e397d58e4ec252bc
Fernando Gómez <gomezhyuuga@gmail.com>
2016-05-05 04:52:05 -0500

    Import of Monsters

--- src/db/seed.rb
+++ src/db/seed.rb
@@ -10,24 +10,43 @@ require_relative '../models/movement'
 # Clean
 Room.dataset.delete
 Movement.dataset.delete
-roomscsv = File.join File.dirname(__FILE__), "rooms.csv"
+Monster.dataset.delete
 
+
+# Read monsters
+monsters_csv = File.join File.dirname(__FILE__), "monsters.csv"
+monsters     = CSV.read(monsters_csv)
+monsters.shift
+
+# Read rooms
+roomscsv = File.join File.dirname(__FILE__), "rooms.csv"
 rooms = CSV.read(roomscsv)
 rooms.shift
+
+# Read movements
 movementscsv = File.join File.dirname(__FILE__), "movements.csv"
 movements = CSV.read(movementscsv)
 movements.shift
+
+# Create models
+puts "Creating monsters..."
+monsters.each do |r|
+  Monster.create(name:     r[0],
+                 ferocity: r[1].to_i)
+end
+puts "#{movements.length} monsters created."
+
+puts "Creating rooms..."
 rooms.each do |r|
-  puts "Creating rooms..."
-  p r
   Room.create(name:        r[0],
               description: r[1],
               monster:     r[2],
-              treasure:    r[3])
+              treasure:    r[3].to_i)
 end
 puts "#{rooms.length} rooms created."
+
+puts "Creating movements..."
 movements.each do |r|
-  puts "Creating movements..."
   Movement.create(room_name: r[0],
                   north:     r[1],
                   south:     r[2],
