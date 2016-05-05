87b84c669bca5f172ac92da086dd81c3dcc16490
Fernando Gómez <gomezhyuuga@gmail.com>
2016-05-05 02:52:43 -0500

    Docs for rooms_status

--- src/models/game.rb
+++ src/models/game.rb
@@ -2,6 +2,15 @@ require_relative "states/exploring_state"
 class Game
   attr_accessor :player, :current_room, :state
 
+  # e.g. rooms_status
+  #   rooms_status = {
+  #     "Hallway": #{ :monster, :treasure }
+  #     ...
+  #   }
+  # If the key +room_name+ is present, means that you have already visited the room
+  # and either killed a monster or picked-up a treasure inside the room.
+  # If the keyword :monster or :treasure is inside the value of the key, it means
+  # that you have killed the monster and/or picked-up the treasure.
   def initialize(player)
     @player = player
     @current_room = "Entrance"
